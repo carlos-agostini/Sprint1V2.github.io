d1ec213e764cd28d861f7adb2a29ed962211b6c8
Carlos Agostini <carlosjr.agostini@gmail.com>
2024-08-22 07:28:17 -0600

    Adding scroll indicator and animations

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,24 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+document.querySelector('.scroll-indicator').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#team').scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    // Añadir la clase para la animación secuencial
+    const boxes = document.querySelectorAll('.team_section .team_container .box');
+    boxes.forEach((box, index) => {
+        setTimeout(() => {
+            box.classList.add('slide-in');
+        }, index * 600); // Intervalo entre cada animación
+    });
+
+    // Opcional: Remover la clase después de la animación para evitar que se repita
+    setTimeout(() => {
+        boxes.forEach(box => box.classList.remove('slide-in'));
+    }, 3000); // Ajusta el tiempo según sea necesario
+});
+
