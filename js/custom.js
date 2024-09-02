8d2cba0a177367ff43062a9d43755bb0405c1e2d
Carlos Agostini <carlosjr.agostini@gmail.com>
2024-09-02 07:50:52 -0600

    Sprint1 development
    Co-authored-by: Jorge Galván González <jgalvanx@users.noreply.github.com>

--- js/custom.js
+++ js/custom.js
@@ -65,3 +65,19 @@ document.querySelector('.scroll-indicator').addEventListener('click', function(e
     }, 3000); // Ajusta el tiempo según sea necesario
 });
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
+
+
+
+
